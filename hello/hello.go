e4b7b80bc36d8ce1272147e3a463e6944fdd1486
Jeffrey XIE <jeffrey.xie@unity3d.com>
2022-02-14 11:13:41 -0500

    Hello Go

---
+++ hello/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote/v4"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+	fmt.Println(quote.Glass())
+}
