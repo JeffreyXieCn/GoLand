bb548ceeb645ba0de589ea2d327e5299217f4cc6
Jeffrey XIE <jeffrey.xie@unity3d.com>
2022-02-28 09:33:35 -0500

    Iterate map entries, keys and values

--- TourOfGo/Basics/more-types/maps.go
+++ TourOfGo/Basics/more-types/maps.go
@@ -36,6 +36,28 @@ func main() {
 	}
 	fmt.Println(m3)
 
+	bl := m3["Bell Labs"]
+	fmt.Println(bl)
+
+	bl.Lat += 1
+	fmt.Println("Bell Labs's Lat changed", bl)
+	fmt.Println("But the map is intact: ", m3)
+
+	fmt.Println("=============Iterate Map Key Values=============")
+	for key, val := range m3 {
+		fmt.Printf("key=%v, value=%v\n", key, val)
+	}
+
+	fmt.Println("=============Iterate Map Keys=============")
+	for key := range m3 {
+		fmt.Printf("key=%v\n", key)
+	}
+
+	fmt.Println("=============Iterate Map Values=============")
+	for _, val := range m3 {
+		fmt.Printf("val=%v\n", val)
+	}
+
 	fmt.Println("=============Mutating Maps=============")
 	m4 := make(map[string]int)
 
